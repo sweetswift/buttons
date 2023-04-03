6581f8df1795615fc02cd0ebb18395fdb156a00a
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-03 23:48:34 +0900

    Add: ElevatedButton 속성들 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -17,6 +17,26 @@ class HomeScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                //메인 컬러
+                backgroundColor: Colors.red,
+                // 글자 및 애니메이션 컬러
+                foregroundColor: Colors.black,
+                // 그림자 컬러
+                shadowColor: Colors.green,
+                //그림자 두께
+                elevation: 10.0,
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                ),
+                padding: EdgeInsets.all(32.0),
+                // 버튼 테두리
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 4.0,
+                ),
+              ),
               child: Text('ElevatedButton'),
             ),
             OutlinedButton(
